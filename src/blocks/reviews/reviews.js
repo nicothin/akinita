9a1a42da38b7bf6ae67b78d8b8f9ef14566fc2cf
Nikolay Gromov <nicothin@gmail.com>
2022-05-03 02:31:25 +0300

    Update 2022-05-02T23:31:18.606Z

--- src/blocks/reviews/reviews.js
+++
@@ -1,8 +0,0 @@
-(function(){
-
-  $('.reviews__more-wrap .more-link').on('click', function(e){
-    e.preventDefault();
-    $(this).closest('.reviews').find('.reviews__list .reviews__item--hidden').slideDown();
-  })
-
-}());
